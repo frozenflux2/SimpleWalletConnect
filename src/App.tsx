0b95c04b9adf95d8c318f2d96995bae040c65b2d
lumi518 <aurora518star.com@gmail.com>
2022-03-22 08:34:33 +0800

    commit all what i did and am going to do as the sample code did

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,38 @@
 import React, { useEffect } from "react";
+import { SigningCosmosClient } from "@cosmjs/launchpad";
+import { Window as KeplrWindow } from "@keplr-wallet/types";
 import "./App.css";
 
 declare global {
-  interface Window {
-    MyNamespace: any;
-  }
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
+  interface Window extends KeplrWindow {}
 }
+
+const CHAIN_ID = "cosmoshub-4";
+// const CHAIN_ID = "osmosis-1";
 
 function App() {
   useEffect(() => {
-    console.log("react app loaded", window);
-    const customWindow: any = window;
-    const result = customWindow.keplr.enable("cosmoshub-4");
-    console.log("result", result);
+    const connectWallet = async () => {
+      console.log("react app loaded", window.keplr);
+      if (!window.keplr) return;
+      await window.keplr.enable(CHAIN_ID);
+      const signer: any = window.keplr.getOfflineSigner(CHAIN_ID);
+      console.log("🚀 ~ file: App.tsx ~ line 13 ~ connectWal ~ signer", signer);
+      const accounts = await signer.getAccounts();
+      console.log(
+        "🚀 ~ file: App.tsx ~ line 17 ~ connectWal ~ accounts",
+        accounts
+      );
+
+      // const cosmJS = new SigningCosmosClient(
+      //   // "https://rpc-cosmoshub.blockapsis.com", // rpc endpoint of osmosis-1
+      //   "https://lcd-cosmoshub.keplr.app",
+      //   accounts[0].address,
+      //   signer
+      // );
+    };
+    connectWallet();
   }, []);
   return <div className="App">Welcom to simple connect wallet app</div>;
 }
